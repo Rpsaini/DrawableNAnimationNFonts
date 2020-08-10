d0a5fd8518704f4f5c27a9bff898313a327c0ea1
Android Developer <developmentapp4@gmail.com>
2020-08-10 18:29:33 +0530

    Animation class added

--- app/src/main/java/com/app/drawablenanimationnfonts/MainActivity.java
+++ app/src/main/java/com/app/drawablenanimationnfonts/MainActivity.java
@@ -2,26 +2,31 @@ package com.app.drawablenanimationnfonts;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.ObjectAnimator;
 import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
-
-
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
-
-
         setContentView(R.layout.activity_main);
+
         // public static final int RECTANGLE = 0;
 //         public static final int OVAL = 1;
 //         public static final int LINE = 2;
 //         public static final int RING = 3;
+
+        ObjectAnimator();
 
 
 //        final Button buttonclick = findViewById(R.id.buttonclick);
@@ -56,6 +61,95 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+
+
+    private void ObjectAnimator()
+    {
+       final EditText editText =findViewById(R.id.editTextBorder);
+       final LinearLayout BottomView =findViewById(R.id.BottomView);
+       final TextView textViewOnclick =findViewById(R.id.textViewOnclick);
+
+       editText.setVisibility(View.GONE);
+
+        textViewOnclick.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                ObjectAnimator  anim = ObjectAnimator.ofFloat(BottomView, "translationY", 0f, -600f);
+                anim.setDuration(1000);//set duration
+                anim.start();//start animation
+                anim.addListener(new Animator.AnimatorListener() {
+                    @Override
+                    public void onAnimationStart(Animator animation)
+                    {
+                    }
+
+                    @Override
+                    public void onAnimationEnd(Animator animation) {
+                        textViewOnclick.setVisibility(View.GONE);
+                        editText.setVisibility(View.VISIBLE);
+                    }
+
+                    @Override
+                    public void onAnimationCancel(Animator animation) {
+
+                    }
+
+                    @Override
+                    public void onAnimationRepeat(Animator animation) {
+
+                    }
+                });
+            }
+        });
+
+
+        findViewById(R.id.buttonclick).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                ObjectAnimator  anim = ObjectAnimator.ofFloat(BottomView, "translationY", -600f, 0f);
+                anim.setDuration(1000);//set duration
+                anim.start();//start animation
+
+                anim.addListener(new AnimatorListenerAdapter() {
+                    @Override
+                    public void onAnimationCancel(Animator animation) {
+                        super.onAnimationCancel(animation);
+                    }
+
+                    @Override
+                    public void onAnimationEnd(Animator animation) {
+                        super.onAnimationEnd(animation);
+                        textViewOnclick.setVisibility(View.VISIBLE);
+                        editText.setVisibility(View.GONE);
+                    }
+
+                    @Override
+                    public void onAnimationRepeat(Animator animation) {
+                        super.onAnimationRepeat(animation);
+                    }
+
+                    @Override
+                    public void onAnimationStart(Animator animation) {
+                        super.onAnimationStart(animation);
+                    }
+
+                    @Override
+                    public void onAnimationPause(Animator animation) {
+                        super.onAnimationPause(animation);
+                    }
+
+                    @Override
+                    public void onAnimationResume(Animator animation) {
+                        super.onAnimationResume(animation);
+                    }
+                });
+            }
+        });
+
+
+    }
 
 
 
